af806eeb65d44613594aa55795033275760325a6
BetaX070 <107304852+BetaX070@users.noreply.github.com>
2026-03-15 12:13:08 +0100

    Change title to 'General Knowledge BIOLOGY AI'

--- chef_gamer.py
+++ chef_gamer.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from groq import Groq
 
-st.title("Generalal Knowledge Geography AI")
+st.title("Generalal Knowledge BIOLOGY AI")
 st.caption("Ask me absolutely anything!")
 
 # Connect to Groq using the Secret Safe
